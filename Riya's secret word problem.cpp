0f67c8acdad718489b5ee98ee26aeb060f479928
karkeedolleepunar-prog <karkeedolleepunar@gmail.com>
2025-11-27 19:17:10 +0530

    Riya's secret word problem .cpp

---
+++ Riya's secret word problem.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string word;
+
+    cout << "Enter a word: ";
+    cin >> word;
+
+    bool isPalindrome = true;
+
+    int start = 0;
+    int end = word.length() - 1;
+
+    while (start < end) {
+        if (word[start] != word[end]) {
+            isPalindrome = false;
+            break;
+        }
+        start++;
+        end--;
+    }
+
+    if (isPalindrome)
+        cout << "Perfect Secret Word" << endl;
+    else
+        cout << "Not a Secret Word" << endl;
+
+    return 0;
+}
