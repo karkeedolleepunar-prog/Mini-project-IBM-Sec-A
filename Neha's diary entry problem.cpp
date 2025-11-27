4701e97baa56f4ff4fbb6315177369ddcf5efd61
karkeedolleepunar-prog <karkeedolleepunar@gmail.com>
2025-11-27 19:18:54 +0530

    Neha's diary entry problem.cpp

---
+++ Neha's diary entry problem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of days: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter exercise times: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Reversed Order: ";
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
