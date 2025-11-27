e0bee7087b6bcf5f8f6ebb993c2c4930ed0e0416
karkeedolleepunar-prog <karkeedolleepunar@gmail.com>
2025-11-27 19:20:54 +0530

    Weather sensor data problem.cpp

---
+++ Weather sensor data problem.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of readings: ";
+    cin >> n;
+
+    int arr[n];  
+
+    cout << "Enter " << n << " temperature readings:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int positive = 0, negative = 0, zero = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > 0)
+            positive++;
+        else if (arr[i] < 0)
+            negative++;
+        else
+            zero++;
+    }
+
+    cout << "Positive = " << positive << endl;
+    cout << "Negative = " << negative << endl;
+    cout << "Zero = " << zero << endl;
+
+    return 0;
+}
